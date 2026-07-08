813307f8ec1af02ae47fefe4804bdaf9b6bc12d4
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2026-07-08 16:13:23 +0600

    added ApiBearerAuth decorator in userController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from 'src/guards/auth/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
